d24567babcfa3ac36a33682d28a265a127de49c6
Richard <1109758969@qq.com>
2026-05-12 00:00:01 +0800

    feat(dataset_manager):add dataset properties, add two new platform function

--- src/dataset/dataset_manager.py
+++ src/dataset/dataset_manager.py
@@ -1,15 +1,7 @@
 from pathlib import Path
 from dataclasses import dataclass
-from typing import List
+from typing import List,Optional
 import json
-# 我想实现的是以下的功能
-# 添加展示名字displayname,
-# 添加是否使用这个数据集标签 isuse/tag, 使用时属性，应该写在数据集配置中吗？
-# 另外的脚本，增删改查这个列表的功能
-# 对数据集的来源进行提示的功能，影响读取/下载/加载的方式
-# 兼容？比如imdb的数据集能用test/split/定界符切片，但是其他来源的数据集可能本身做不到这一点。
-# 添加数据集的时候，是否要尝试读取内容进行属性的确定？
-
 
 # 获取本地数据集存储地址
 from config import DATASET_PATH
@@ -23,6 +15,13 @@ class Dataset:
     remote_path:str
     local_path:str
     enabled:bool = True
+    auth_token: Optional[str] = None
+    source_path:Optional[str] = None # 子路径下载  这个不太懂
+    revision: Optional[str] = None # 版本号
+    allow_patterns:Optional[List[str]] = None # 需要下载的文件过滤
+    ignore_patterns:Optional[List[str]] = None # 不需要下载的文件过滤
+
+
 
     @property
     def full_local_path(self) -> Path:
@@ -30,9 +29,74 @@ class Dataset:
 
 class DatasetDownloader:
 
-    def __init__(self,path:Path):
-        self.path = DATASET_PATH
+    def __init__(self,dataset:Dataset):
+        self.dataset = dataset
+        self.path = self.dataset.full_local_path
+        self.permitplatform = ['huggingface','ModelScope','']
+        
+
+    def execute(self):
+
         self.path.mkdir(parents=True,exist_ok=True)
+        download_func = self._get_download_function()
+        assert download_func is not None
+        # TODO 对download_func为None时进行错误处理
+        try:
+            download_func(
+                repo_id = self.dataset.name,
+                local_dir = self.path,
+                local_dir_use_symlinks=False,
+            )
+            pass
+        except Exception as e:
+            pass
+        pass
+
+    def _get_download_function(self):
+        # TODO 加入模糊/近似匹配
+        platform = self.dataset.platform
+        if platform not in self.permitplatform:
+            print('不支持该平台')
+            print(f"当前支持的平台有{','.join(self.permitplatform)}")
+            return None
+        if platform == "huggingface":
+            try:
+                # from huggingface_hub import hf_hub_download
+                from huggingface_hub import snapshot_download
+                """参数参考
+                snapshot_download(
+                    repo_id="username/dataset_name",
+                    repo_type="dataset",           # 仓库类型
+                    revision="main",               # 版本分支/commit
+                    local_dir="/path/to/save",     # 目标目录（⚠️ 注意语义）
+                    cache_dir="/path/to/cache",    # 缓存目录
+                    allow_patterns=["*.json"],     # 只下载匹配的文件
+                    ignore_patterns=["*.pt"],      # 忽略的文件
+                    token="hf_xxx",                # 认证token（私有数据集需要）
+                    resume_download=True,          # 断点续传
+                    local_dir_use_symlinks=False,  # 不使用软链接
+                )"""
+                return snapshot_download
+            except ImportError as e:
+                print(f"缺少huggingface_hub，使用pip install huggingface_hub 进行下载")
+        elif platform == "ModelScope":
+            try:
+                from modelscope import snapshot_download
+                return snapshot_download
+            except ImportError as e:
+                print(f"缺少modelscope库，请使用pip install modelscope进行安装")
+        elif platform == "Kaggle":
+            try:
+                from kagglehub import dataset_download
+                return dataset_download
+            except ImportError as e:
+                print(f"缺少kagglehub库，请使用pip install kagglehub进行安装")
+        else:
+            def fun(**kwargs):
+                print('意外情况')
+                return
+            return fun
+
 
 class DatasetLoader:
     def __init__(self,path:Path):
@@ -71,11 +135,14 @@ class DatasetManager:
             json.dump([d.__dict__ for d in self.datasets], f, indent=2, ensure_ascii=False)
 
     # CRUD
-    def add(self,dataset:Dataset):
+    def add(self,dataset:Dataset,downloadnow:bool = True):
         if any(d.name ==dataset.name for d in self.datasets):
             raise ValueError(f"数据集{dataset.name}已存在")
         self.datasets.append(dataset)
         self._save()
+        if downloadnow:
+            downloader = DatasetDownloader(dataset = dataset)
+            downloader.execute()
     
     def get(self,name:str):
         for d in self.datasets:
@@ -91,11 +158,10 @@ class DatasetManager:
         self._save()
 
     def update(self,name:str,**kwargs):
+        # name作为键值，不得重复，不得修改
         ds = self.get(name)
         if ds is None:
             raise KeyError(f"数据集{name}不存在")
-        # TODO 更新逻辑存在一定问题
-        # TODO 未处理名字替换时，已有同名名字的……呃，可能有点麻烦
         for key,value in kwargs.items():
             if hasattr(ds,key):
                 setattr(ds,key,value)
